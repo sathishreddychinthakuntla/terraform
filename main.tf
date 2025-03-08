07d7c8446806947a7f165cd0b2518c50fbd59c70
sathishreddychinthakuntla <sathishreddych96@gmail.com>
2025-03-08 18:26:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_instance" "one" {
-ami = "ami-0f2ce9ce760bd7133"
+ami = "ami-08fe5144e4659a3b3"
 instance_type = "t2.micro"
 tags = {
 Name = "deva"
